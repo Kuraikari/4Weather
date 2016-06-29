85d8cb14fceeec777931c4b908810ee202a68d32
Zian  Wermelinger <zwermi@gmail.com>
2016-06-29 14:00:52 +0200

    GPSLocation (IGNORE PLS)
    It went wrong just ignore i'll delete it later

---
+++ src/GPSLocation/app/src/main/java/ch/zario/gpslocation/GPSLocation.java
@@ -0,0 +1,16 @@
+package ch.zario.gpslocation;
+
+import android.app.Activity;
+
+/**
+ * Created by student on 29.06.2016.
+ */
+public class GPSLocation extends Activity{
+
+
+
+    public void hoidcansur(){
+
+    }
+
+}
